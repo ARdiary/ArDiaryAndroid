c2b1af0f9499c8cb731804cc40be5b6950a4ff17
Zzu-h <juho7785@naver.com>
2022-09-01 15:17:44 +0900

    feat: DataBinding 추가 및 marker Preview Custom View 구현
    - DataBinding 추가
    - Marker Preview 커스텀 뷰 구현
       - url과 resource 두 가지 방식을 통해 이미지를 세팅할 수 있음
    - 테스트를 위해 library를 application으로 임시 전환

--- newdiary/src/main/java/com/army/newdiary/ui/common/custom/MarkerPreview.kt
+++ newdiary/src/main/java/com/army/newdiary/ui/common/custom/MarkerPreview.kt
@@ -2,18 +2,44 @@ package com.army.newdiary.ui.common.custom
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.databinding.DataBindingUtil
+import com.army.newdiary.R
+import com.army.newdiary.databinding.ViewMarkerPreviewBinding
+import com.bumptech.glide.Glide
 
 class MarkerPreview(
     context: Context,
     attrs: AttributeSet?
 ) : LinearLayout(context, attrs) {
 
-    fun setMarkerImage(url: String) {
+    private val imageList: List<ImageView>
+    private val binding: ViewMarkerPreviewBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context),
+        R.layout.view_marker_preview,
+        this,
+        true
+    )
 
+    init {
+        imageList = listOf(binding.defaultImageIv, binding.markerImageIv, binding.shadowImageIv)
+    }
+
+    fun setMarkerImage(url: String) {
+        imageList.forEach {
+            Glide.with(context)
+                .load(url)
+                .into(it)
+        }
     }
 
     fun setMarkerImage(id: Int) {
-
+        imageList.forEach {
+            Glide.with(context)
+                .load(id)
+                .into(it)
+        }
     }
 }
