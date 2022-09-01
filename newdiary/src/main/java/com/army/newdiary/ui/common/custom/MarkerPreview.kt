4f0105d8c70f33c98d96f87cd4667e20cee7c5c5
Zzu-h <juho7785@naver.com>
2022-09-01 13:49:58 +0900

    ui: UI 다듬기 (필요로 되는 이미지 추가)
    - 필요한 이미지를 추가 및 뷰 생성

---
+++ newdiary/src/main/java/com/army/newdiary/ui/common/custom/MarkerPreview.kt
@@ -0,0 +1,19 @@
+package com.army.newdiary.ui.common.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class MarkerPreview(
+    context: Context,
+    attrs: AttributeSet?
+) : LinearLayout(context, attrs) {
+
+    fun setMarkerImage(url: String) {
+
+    }
+
+    fun setMarkerImage(id: Int) {
+
+    }
+}
