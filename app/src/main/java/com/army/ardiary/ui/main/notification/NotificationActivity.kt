953586fa7d8e1da7818fd57622f7dea7bc5f00ba
Zzu-h <juho7785@naver.com>
2022-10-17 23:32:02 +0900

    feat: Notification List Adapter 연결 및 Databinding Data 연결
    - 리스트 어댑터 연결 및 뷰 적용
    - 데이터 바인딩 데이터 연결

--- app/src/main/java/com/army/ardiary/ui/main/notification/NotificationActivity.kt
+++ app/src/main/java/com/army/ardiary/ui/main/notification/NotificationActivity.kt
@@ -4,6 +4,8 @@ import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.army.ardiary.databinding.ActivityNotificationBinding
 import com.army.ardiary.ui.BaseActivity
+import com.army.ardiary.ui.common.viewutils.showContent
+import com.army.ardiary.ui.main.notification.adapter.NotificationRVAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -13,14 +15,28 @@ class NotificationActivity() :
     BaseActivity<ActivityNotificationBinding>(ActivityNotificationBinding::inflate) {
 
     private val viewModel by viewModels<NotificationViewModel>()
+    private lateinit var notificationRVAdapter: NotificationRVAdapter
 
     override fun initAfterBinding() {
+        initAdapter()
         initObserver()
+        initButton()
+    }
+
+    private fun initButton() {
+        binding.btnBack.setOnClickListener { finish() }
+    }
+
+    private fun initAdapter() {
+        notificationRVAdapter = NotificationRVAdapter()
+        binding.rvNotCheck.adapter = notificationRVAdapter
     }
 
     private fun initObserver() {
         viewModel.notificationList.onEach {
-            showToast(it.toString())
+            notificationRVAdapter.submitList(it) {
+                with(binding) { showContent(llContent, pbLoading, evError) }
+            }
         }.launchIn(this.lifecycleScope)
     }
 }
