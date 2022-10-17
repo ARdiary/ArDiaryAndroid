f91380636032ff8b4d3c3c5f8972dc0ffffa6ac3
Zzu-h <juho7785@naver.com>
2022-10-17 17:20:42 +0900

    ui: TimeCapsule View 구현
    - 안드로이드 뷰 구현

--- app/src/main/java/com/army/ardiary/ui/main/diary/timecasulelist/TimeCapsuleListActivity.kt
+++ app/src/main/java/com/army/ardiary/ui/main/diary/timecasulelist/TimeCapsuleListActivity.kt
@@ -3,7 +3,9 @@ package com.army.ardiary.ui.main.diary.timecasulelist
 import com.army.ardiary.databinding.ActivityTimeCapsuleListBinding
 import com.army.ardiary.ui.BaseActivity
 
-class TimeCapsuleListActivity: BaseActivity<ActivityTimeCapsuleListBinding>(ActivityTimeCapsuleListBinding::inflate) {
+class TimeCapsuleListActivity :
+    BaseActivity<ActivityTimeCapsuleListBinding>(ActivityTimeCapsuleListBinding::inflate) {
     override fun initAfterBinding() {
+
     }
 }
