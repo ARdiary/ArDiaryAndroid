634a40d849623586a37032437a2faf220413dddd
Zzu-h <juho7785@naver.com>
2022-10-17 22:40:19 +0900

    ui: Notification 화면 구성
    - view 구현

--- app/src/main/java/com/army/ardiary/ui/main/notification/NotificationActivity.kt
+++ app/src/main/java/com/army/ardiary/ui/main/notification/NotificationActivity.kt
@@ -2,7 +2,9 @@ package com.army.ardiary.ui.main.notification
 
 import com.army.ardiary.databinding.ActivityNotificationBinding
 import com.army.ardiary.ui.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NotificationActivity() :
     BaseActivity<ActivityNotificationBinding>(ActivityNotificationBinding::inflate) {
 
